db0f0e6ee5902c4267e4f020e98e2a267d128d90
erkartali <erkartali@gmail.com>
2020-05-21 19:50:47 -0500

    added javascript to trun arrow and change color on button select accordion page

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -1,10 +1,78 @@
-$(".accordion-button").on("click", function() {
-    if ($(this).hasClass("collapsed")) {
-        if ($("button").attr("aria-expanded") === 'false') {
-          $(".accordion-button").css("color", "rgb(128, 128, 128)");
-        }
-        $(this).css("color", "#c3002f");
+$(".button-1").on("click", function() {
+  $(".button-2").removeClass("opened-color");
+  $(".arrow-2").removeClass("opened-arrow");
+
+  $(".button-3").removeClass("opened-color");
+  $(".arrow-3").removeClass("opened-arrow");
+
+  $(".button-4").removeClass("opened-color");
+  $(".arrow-4").removeClass("opened-arrow");
+
+  if ($(".button-1").hasClass("opened-color")) {
+    $(".button-1").removeClass("opened-color");
+    $(".arrow-1").removeClass("opened-arrow");
   } else {
-    $(this).css("color", "rgb(128, 128, 128)");
+    $(".button-1").addClass("opened-color");
+    $(".arrow-1").addClass("opened-arrow");
+  }
+});
+
+$(".button-2").on("click", function() {
+
+  $(".button-1").removeClass("opened-color");
+  $(".arrow-1").removeClass("opened-arrow");
+
+  $(".button-3").removeClass("opened-color");
+  $(".arrow-3").removeClass("opened-arrow");
+
+  $(".button-4").removeClass("opened-color");
+  $(".arrow-4").removeClass("opened-arrow");
+
+  if ($(".button-2").hasClass("opened-color")) {
+    $(".button-2").removeClass("opened-color");
+    $(".arrow-2").removeClass("opened-arrow");
+  } else {
+    $(".button-2").addClass("opened-color");
+    $(".arrow-2").addClass("opened-arrow");
+  }
+});
+
+$(".button-3").on("click", function() {
+
+  $(".button-1").removeClass("opened-color");
+  $(".arrow-1").removeClass("opened-arrow");
+
+  $(".button-2").removeClass("opened-color");
+  $(".arrow-2").removeClass("opened-arrow");
+
+  $(".button-4").removeClass("opened-color");
+  $(".arrow-4").removeClass("opened-arrow");
+
+  if ($(".button-3").hasClass("opened-color")) {
+    $(".button-3").removeClass("opened-color");
+    $(".arrow-3").removeClass("opened-arrow");
+  } else {
+    $(".button-3").addClass("opened-color");
+    $(".arrow-3").addClass("opened-arrow");
+  }
+});
+
+$(".button-4").on("click", function() {
+
+$(".button-1").removeClass("opened-color");
+  $(".arrow-1").removeClass("opened-arrow");
+
+  $(".button-2").removeClass("opened-color");
+  $(".arrow-2").removeClass("opened-arrow");
+
+  $(".button-3").removeClass("opened-color");
+  $(".arrow-3").removeClass("opened-arrow");
+
+  if ($(".button-4").hasClass("opened-color")) {
+    $(".button-4").removeClass("opened-color");
+    $(".arrow-4").removeClass("opened-arrow");
+  } else {
+    $(".button-4").addClass("opened-color");
+    $(".arrow-4").addClass("opened-arrow");
   }
 });
